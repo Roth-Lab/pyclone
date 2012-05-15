38d174c2e04e252bdc73d95c52e95770fa80901e
aroth85 <aroth85@localhost>
2012-05-15 11:35:00 -0700

    Updated sampler results object to make compatible with PyPy.

--- lib/pyclone/results.py
+++ lib/pyclone/results.py
@@ -3,21 +3,40 @@ Created on 2012-05-10
 
 @author: Andrew
 '''
+import os
+import platform
 import shelve
 
 class SamplerResults(object):
-    def __init__(self, file_name, mode='r', max_cache_size=1000):
-        self._db = shelve.open(file_name, writeback=True)
-        
-        if mode == 'w':                
-            self._db['trace'] = {'alpha' : [], 'labels' : [], 'phi' : []}
-            
-            self._db.sync()
-            
+    def __init__(self, file_prefix, max_cache_size=1000):
+        self._load_db(file_prefix)
+
         self._cache_size = 0
             
         self._max_cache_size = max_cache_size
     
+    def _load_db(self, file_prefix):
+        '''
+        Load the shelve db object if it exists, otherwise initialise.
+        ''' 
+        # Workaround for PyPY shelve implementation adding .db and Cpython not
+        if platform.python_implementation() == "PyPy":
+            file_name = file_prefix 
+        else:
+            file_name += ".db"
+        
+        self._db = shelve.open(file_name, writeback=True)
+        
+        # Check if file exists, if not initialise
+        if not os.path.exists(file_name):                
+            self._db['trace'] = {'alpha' : [], 'labels' : [], 'phi' : []}
+            
+            self._db.sync()
+        else:
+            # If file exists and trace is not in db raise error.
+            if 'trace' not in self._db:
+                raise Exception('{0} exists but does not appear to be a valid PyClone SamplerResults object.'.format(file_name))
+                
     def __getitem__(self, key):
         return self._db[key]
     
