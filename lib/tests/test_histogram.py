c41201fa52e8f73c9bc00f8aa993cb6bad7401ca
aroth85 <aroth85@localhost>
2012-06-04 09:59:49 -0700

    Updated histogram unittest to make use of new return values.

--- lib/tests/test_histogram.py
+++ lib/tests/test_histogram.py
@@ -14,14 +14,14 @@ class Test(unittest.TestCase):
     def test_left_endpoint(self):
         values = [-1, ]
         
-        counts = histogram(values, num_bins=2, min_value= -1, max_value=1)
+        bin_centres, counts = histogram(values, num_bins=2, min_value= -1, max_value=1)
         
         self.assertEqual(counts, [1, 0])
 
     def test_right_endpoint(self):
         values = [1, ]
         
-        counts = histogram(values, num_bins=2, min_value= -1, max_value=1)
+        bin_centres, counts = histogram(values, num_bins=2, min_value= -1, max_value=1)
         
         self.assertEqual(counts, [0, 1])
 
@@ -31,7 +31,7 @@ class Test(unittest.TestCase):
         for left, right in [(-2, -1), (-0.5, 0.5), (1, 2), (100, 1000)]:    
             values = self._generate_random_values(left, right, 1000)
             
-            counts = histogram(values, num_bins=bins, min_value=left, max_value=right)
+            bin_centres, counts = histogram(values, num_bins=bins, min_value=left, max_value=right)
             np_counts = list(np.histogram(values, bins=bins, range=(left, right))[0])
             
             self.assertEqual(np_counts, counts)
@@ -42,7 +42,7 @@ class Test(unittest.TestCase):
         for left, right in [(-2, -1), (-0.5, 0.5), (1, 2), (100, 1000)]:    
             values = self._generate_random_values(left, right, 1000)
             
-            counts = histogram(values, num_bins=bins, min_value=left, max_value=right, normalise=True)
+            bin_centres, counts = histogram(values, num_bins=bins, min_value=left, max_value=right, normalise=True)
             np_counts = list(np.histogram(values, bins=bins, range=(left, right), density=True)[0])
             
             for x, y in zip(counts, np_counts):
@@ -51,14 +51,14 @@ class Test(unittest.TestCase):
     def test_values_less_than_min(self):
         values = [-0.01, ]
         
-        counts = histogram(values, num_bins=2, min_value= 0, max_value=1)
+        bin_centres, counts = histogram(values, num_bins=2, min_value= 0, max_value=1)
         
         self.assertEqual(counts, [0, 0])
     
     def test_values_less_than_max(self):
         values = [1.01, ]
         
-        counts = h`istogram(values, num_bins=2, min_value= 0, max_value=1)
+        bin_centres, counts = histogram(values, num_bins=2, min_value= 0, max_value=1)
         
         self.assertEqual(counts, [0, 0])
     
