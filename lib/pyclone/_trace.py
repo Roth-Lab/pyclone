ccca42a981336327ff9bb746b18018a510461c19
aroth85 <aroth85@localhost>
2012-08-19 12:17:19 -0700

    - Removed post processor module and move trace post processor to _trace module

--- lib/pyclone/_trace.py
+++ lib/pyclone/_trace.py
@@ -3,6 +3,8 @@ Created on 2012-05-10
 
 @author: Andrew
 '''
+from collections import defaultdict
+
 import os
 
 import pyclone.zshelve as shelve
@@ -61,3 +63,67 @@ class TraceDB(object):
     
     def sync(self):
         self._db.sync()
+
+class TracePostProcessor(object):
+    def __init__(self, trace_db):       
+        self.genes = trace_db['genes']
+        
+        self._db = trace_db
+        
+        self._results = trace_db['trace']
+        
+    @property
+    def alpha(self):
+        '''
+        Returns a list of alpha values of each iteration of the MCMC chain.
+        '''
+        return self._results['alpha']
+    
+    @property
+    def cellular_frequencies(self):
+        '''
+        Returns a dictionary with keys genes, and values posterior samples of cellular frequencies.
+        '''
+        phi = defaultdict(list)
+        
+        labels = self.labels
+        
+        for gene in labels:
+            for label, sample in zip(labels[gene], self._results['phi']):
+                phi[gene].append(sample[label])
+        
+        return phi
+
+    @property
+    def labels(self):
+        '''
+        Returns a dict with keys genes, and values the class label of the genes for each MCMC sample.
+        '''
+        labels = defaultdict(list)
+        
+        for sample in self._results['labels']:
+            for gene, label in zip(self.genes, sample):
+                labels[gene].append(label)
+        
+        return labels
+
+    @property
+    def num_components(self):
+        '''
+        Returns a list of the number of components used in by each MCMC sample.
+        '''
+        labels = self._results['labels']
+        
+        num_components = []
+        
+        for sample in labels:
+            num_components.append(len(set(sample)))
+        
+        return num_components
+    
+    @property
+    def num_iterations(self):
+        '''
+        Returns the number of MCMC iterations.
+        '''
+        return self._db['sampler'].num_iters
