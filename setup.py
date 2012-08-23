293de7c3674be2b48aeb5fa7fda3b6c47c2d2a81
aroth85 <aroth85@localhost>
2012-08-23 15:40:55 -0700

    - Updated version number. - Reverted scipy integral

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
       name='PyClone',
-      version='0.3.0',
+      version='0.3.1',
       description='Python tools for analysing clonal evolution using NGS data.',
       author='Andrew Roth',
       author_email='andrewjlroth@gmail.com',
