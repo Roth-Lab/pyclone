9e2f9c776bea94c27d9faa59745db25086a66306
aroth85 <aroth85@localhost>
2012-10-23 13:22:54 -0700

    Starting 'gibbs' branch

--- lib/pyclone/sampler.py
+++ lib/pyclone/sampler.py
@@ -6,17 +6,22 @@ Created on 2011-12-15
 from __future__ import division
 
 from collections import namedtuple
+from math import log
 
 from pydp.base_measures import BetaBaseMeasure
 
 from pydp.densities import Density, log_binomial_pdf
 from pydp.partition import Partition
+from pydp.rvs import uniform_rvs
 
-from pydp.samplers.atom import BaseMeasureAtomSampler
+from pydp.proposal_functions import ProposalFunction
+
+from pydp.samplers.atom import AtomSampler, BaseMeasureAtomSampler, MetropolisHastingsAtomSampler
 from pydp.samplers.concentration import GammaPriorConcentrationSampler
 from pydp.samplers.partition import AuxillaryParameterPartitionSampler
 
-from pydp.utils import log_sum_exp
+from pydp.utils import log_sum_exp, memoized
+from pydp.data import BetaParameter, BetaData
 
 class DirichletProcessSampler(object):
     def __init__(self, alpha=None):
@@ -26,7 +31,7 @@ class DirichletProcessSampler(object):
         
         self.partition_sampler = AuxillaryParameterPartitionSampler(self.base_measure, cluster_density)
         
-        self.atom_sampler = BaseMeasureAtomSampler(self.base_measure, cluster_density)           
+        self.atom_sampler = PyCloneGibbsAtomSampler(self.base_measure, cluster_density)           
         
         if alpha is None:
             self.alpha = 1
@@ -88,11 +93,147 @@ class DirichletProcessSampler(object):
             self.partition.add_cell(base_measure.random())
             
             self.partition.add_item(item, item)
+
+class StromalSampler(DirichletProcessSampler):
+    def __init__(self, alpha=None):
+        self.base_measure = BetaBaseMeasure(1, 1)
+        
+        cluster_density = PyCloneStromalDensity()
+        
+        self.partition_sampler = AuxillaryParameterPartitionSampler(self.base_measure, cluster_density)
+        
+        proposal_func = PyCloneProposalFunction()
+        
+        self.atom_sampler = MetropolisHastingsAtomSampler(self.base_measure, cluster_density, proposal_func)
+        
+        self.s = 0.01           
+        
+        if alpha is None:
+            self.alpha = 1
             
+            self.update_alpha = True
+            
+            self.concentration_sampler = GammaPriorConcentrationSampler(1, 1)
+        else:
+            self.alpha = alpha
+            
+            self.update_alpha = False 
+        
+        self.num_iters = 0
+    
+    @property
+    def state(self):
+        return {
+                'alpha' : self.alpha,
+                'cellular_frequencies' : [param.x for param in self.partition.item_values],
+                'labels' : self.partition.labels,
+                'phi' : [param.x for param in self.partition.cell_values],
+                's' : self.s
+                }
+        
+    def interactive_sample(self, data):
+        if self.update_alpha:
+            self.alpha = self.concentration_sampler.sample(self.alpha,
+                                                           self.partition.number_of_cells,
+                                                           self.partition.number_of_items)
+        
+        self.partition_sampler.sample(data, self.partition, self.alpha)
+        
+        self.atom_sampler.sample(data, self.partition)
+        
+        self.update_normal_proportion(data, self.partition)
+        
+    def update_normal_proportion(self, data, partition):
+        old_ll = 0
+        new_ll = 0
+        
+        new_s = uniform_rvs(0, 1)
+        
+        for cell in partition.cells:
+            old_param = cell.value
+            
+            new_param = old_param.copy()
+            new_param.s = new_s
+
+            for j in cell.items:
+                old_ll += self.cluster_density.log_p(data[j], old_param)
+                new_ll += self.cluster_density.log_p(data[j], new_param)
+            
+        log_numerator = new_ll
+        log_denominator = old_ll
+            
+        log_ratio = log_numerator - log_denominator
+            
+        u = uniform_rvs(0, 1)
+            
+        if log_ratio >= log(u):
+            self.s = new_s
+            
+            for cell in partition.cells:
+                cell.value.s = new_s
+                
+    def _init_partition(self, base_measure):
+        self.partition = Partition()
+        
+        for item, _ in enumerate(self.data):
+            self.partition.add_cell(base_measure.random())
+            
+            self.partition.add_item(item, item)                   
+
+class PyCloneProposalFunction(ProposalFunction):
+    def log_p(self, data, params):
+        return 0
+    
+    def random(self, params):
+        phi = uniform_rvs(0, 1)
+        
+        return PyCloneParameter(phi, params.s)
+
+class PyCloneGibbsAtomSampler(AtomSampler):
+    def sample(self, data, partition):
+        for cell in partition.cells:
+            log_f = lambda x: sum([self.cluster_density.log_p(data[item], x) for item in cell.items])
+            
+            phi = self.inverse_sample(log_f, 0, 1, 100)
+            
+            cell.value = phi
+            
+    def inverse_sample(self, log_f, a, b, mesh_size):
+        u = uniform_rvs(0, 1)
+        
+        log_u = log(u)
+        
+        step_size = (b - a) / mesh_size
+        
+        log_step_size = log(b - a) - log(mesh_size)
+    
+        knots = [i * step_size + a for i in range(0, mesh_size + 1)]
+        knots = [BetaData(x) for x in knots]
+    
+        log_total = [log_f(x) + log_step_size for x in knots]
+    
+        log_norm_const = log_sum_exp(log_total)
+        
+        log_cdf = None
+        
+        for x in knots:
+            term = log_f(x) - log_norm_const
+            
+            if log_cdf is None:
+                log_cdf = term
+            else:
+                log_cdf = log_sum_exp([log_cdf, term + log_step_size])
+
+            if log_u < log_cdf:
+                return x
+        
+        return x            
+
 #=======================================================================================================================
 # Data class
 #=======================================================================================================================
 PyCloneData = namedtuple('PyCloneData', ['a', 'd', 'mu_r', 'mu_v', 'log_pi_r', 'log_pi_v'])
+PyCloneParameter = namedtuple('PyCloneParameter', ['phi', 's'])
 
 class PyCloneDensity(Density):
     def log_p(self, data, params):
@@ -110,3 +251,20 @@ class PyCloneDensity(Density):
         mu = (1 - phi) * mu_r + phi * mu_v
         
         return log_binomial_pdf(a, d, mu)
+    
+class PyCloneStromalDensity(Density):
+    def log_p(self, data, params):
+        ll = []
+        
+        for mu_r, log_pi_r in zip(data.mu_r, data.log_pi_r):
+            for mu_v, log_pi_v in zip(data.mu_v, data.log_pi_v):
+                temp = log_pi_r + log_pi_v + self._log_binomial_likelihood(data.a, data.d, params.phi, params.s, mu_r, mu_v)
+                
+                ll.append(temp)
+        
+        return log_sum_exp(ll)
+    
+    def _log_binomial_likelihood(self, a, d, phi, s, mu_r, mu_v):
+        mu = (1 - phi * (1 - s)) * mu_r + phi * (1 - s) * mu_v
+        
+        return log_binomial_pdf(a, d, mu)    
--- lib/test/test_simulator.py
+++ lib/test/test_simulator.py
@@ -5,62 +5,23 @@ Created on 2012-09-15
 '''
 from __future__ import division
 
-import random
+from random import gammavariate as gamma_rvs
 
-from random import betavariate as beta_rvs, gammavariate as gamma_rvs, normalvariate as normal_rvs
+from pyclone.run import get_log_mix_weights
+from pyclone.sampler import PyCloneData, DirichletProcessSampler
 
-from pyclone.utils import discrete_rvs, dirichlet_rvs, binomial_rvs
+from pydp.base_measures import BetaBaseMeasure
+from pydp.partition import Partition
+from pydp.rvs import binomial_rvs, discrete_rvs, dirichlet_rvs
+from pydp.tests.simulators import sample_from_crp
+from pydp.tests.geweke import compare_trace
 
-from pyclone.sampler import PyCloneData, Partition, sample_alpha, sample_partition, sample_cell_values
-
-from pyclone.test.crp import sample_from_crp
-from pyclone.test.geweke import compare_trace
-
-def normal_proposal(old_phi):
-    while True:
-        new_phi = normal_rvs(old_phi, 1)
-        
-        if 0 <= new_phi <= 1:
-            return new_phi
-
-def beta_proposal(m):
-    if m == 0 or m == 1:
-        return random.random()
-    
-    s = 10
-    
-    a = s * m
-    b = s - a
-    
-    if a <= 0:
-        a = 1e-100
-    if b <= 0:
-        b = 1e-100
-
-    return beta_rvs(a, b)  
-
-def uniform_proposal(phi):
-    return random.random()
-
-#=======================================================================================================================
-# Heleper Simulators
-#=======================================================================================================================
-def draw_from_prior(a, b, delta_v):
+def draw_from_prior(a, b, base_measure, delta_v):
     size = len(delta_v)
     
-#    alpha = 1
     alpha = gamma_rvs(a, b)
     
-    labels, values = sample_from_crp(alpha, size, random.random)
-    
-    partition = Partition()
-    
-    for k, value in enumerate(values):
-        partition.add_cell(value)
-        
-        for item, cell_index in enumerate(labels):
-            if cell_index == k:
-                partition.add_item(item, cell_index)
+    partition = sample_from_crp(alpha, size, base_measure)
 
     return alpha, partition
 
@@ -76,17 +37,17 @@ def draw_data(partition, mu_r, mu_v, delta_v):
     
     data = []
     
-    phi = partition.item_values
+    phi = [params.x for params in partition.item_values]
     
     for i in range(len(phi)):
         mu = (1 - phi[i]) * mu_r + phi[i] * mu_v[g[i]]
         
         a = binomial_rvs(d, mu)
         
-        data_point = PyCloneData(a,
-                                 d,
-                                 {mu_r : 1},
-                                 dict(zip(mu_v, delta_v[i])))
+        log_pi_r = get_log_mix_weights([1, ])
+        log_pi_v = get_log_mix_weights(delta_v[i])
+        
+        data_point = PyCloneData(a, d, tuple([mu_r, ]), tuple(mu_v), tuple(log_pi_r), tuple(log_pi_v))
         
         data.append(data_point)
     
@@ -95,61 +56,64 @@ def draw_data(partition, mu_r, mu_v, delta_v):
 def marginal_conditional_simulator(a, b, mu_r, mu_v, delta_v, num_iters): 
     params = []
     
+    base_measure = BetaBaseMeasure(1, 1)
+    
     for _ in range(num_iters):
-        alpha, partition = draw_from_prior(a, b, delta_v) 
+        alpha, partition = draw_from_prior(a, b, base_measure, delta_v) 
         
-        params.append({'alpha' : alpha, 'phi' : partition.item_values})
+        params.append({
+                       'alpha' : alpha,
+                       'cellular_frequencies' : [value.x for value in partition.item_values]
+                       })
         
         draw_data(partition, mu_r, mu_v, delta_v)
     
     return params
 
 def successive_conditional_simulator(a, b, mu_r, mu_v, delta_v, num_iters):
-    params_sample = []
+    params = []
     
-    alpha, partition = draw_from_prior(a, b, delta_v)
+    base_measure = BetaBaseMeasure(1, 1)
+    
+    alpha, partition = draw_from_prior(a, b, base_measure, delta_v)
+    
+    sampler = DirichletProcessSampler()
+    
+    data = draw_data(partition, mu_r, mu_v, delta_v)
+    
+    sampler.partition = partition
     
     for _ in range(num_iters):
+        sampler.interactive_sample(data)
+
+        params.append(sampler.state)
+        
         data = draw_data(partition, mu_r, mu_v, delta_v)
-        
-#        new_alpha = 1
-        new_partition = sample_partition(alpha, data, partition, random.random)
-        
-        new_alpha = sample_alpha(alpha,
-                                 new_partition.number_of_cells,
-                                 new_partition.number_of_items,
-                                 a=a,
-                                 b=b)
-        
-#        new_partition = partition     
-        
-        
-        sample_cell_values(data, new_partition, uniform_proposal)
-        
-        params = {'alpha' : new_alpha, 'phi' : new_partition.item_values}
-        
-        params_sample.append(params)
-        
-        alpha = new_alpha
-        
-        partition = new_partition
     
-    return params_sample
+    return params
     
 if __name__ == "__main__":
     delta_v = [
-               [1, 1],
-               [1, 1]
+               [1, ],
+               [1, ],
+               [1, ],
+               [1, ],
+               [1, ],
+               [1, ],
+               [1, ],
+               [1, ],
+               [1, ],
+               [1, ]
                ]
 
     mu_r = 0.999    
-    mu_v = [0.001, 0.5]
+    mu_v = [0.001, ]
     
     a = 1
     b = 1
     
-    burnin = 90000
-    num_iters = 100000
+    num_iters = int(2e6)
+    burnin = int(1e6)
     thin = 100
     
     params_1 = marginal_conditional_simulator(a, b, mu_r, mu_v, delta_v, num_iters)
@@ -158,12 +122,12 @@ if __name__ == "__main__":
     trace_1 = [x['alpha'] for x in params_1][burnin::thin]
     trace_2 = [x['alpha'] for x in params_2][burnin::thin]
     
-    print compare_trace(trace_1, trace_2, lambda x: x)
-    print compare_trace(trace_1, trace_2, lambda x: x ** 2)
+#    print compare_trace(trace_1, trace_2, lambda x: x)
+#    print compare_trace(trace_1, trace_2, lambda x: x ** 2)
     
     for i in range(len(delta_v)):
-        trace_1 = [x['phi'][i] for x in params_1][burnin::thin]
-        trace_2 = [x['phi'][i] for x in params_2][burnin::thin]
+        trace_1 = [x['cellular_frequencies'][i] for x in params_1][burnin::thin]
+        trace_2 = [x['cellular_frequencies'][i] for x in params_2][burnin::thin]
         
         print compare_trace(trace_1, trace_2, lambda x: x)
         print compare_trace(trace_1, trace_2, lambda x: x ** 2)
