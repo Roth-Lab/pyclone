4239bcbd93c9cb132eebe697c6036429568dbc16
aroth85 <aroth85@localhost>
2013-02-11 12:01:33 -0800

    Updated version.

---
+++ README.txt
@@ -0,0 +1,17 @@
+# Installation
+
+## Dependencies
+
+* [numpy 1.6.2](http://www.numpy.org)
+
+### Required
+
+* [brewer2mpl 1.0] (https://github.com/jiffyclub/brewer2mpl)
+
+* [maplotlib 1.2.0](http://matplotlib.org)
+
+* [rpy2 2.3.3](http://rpy.sourceforge.net/rpy2.html)
+
+* [scikits-learn 0.13](http://scikit-learn.org)
+
+* [scipy 0.11](http://www.scipy.org)
--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from distutils.core import setup
 
 setup(
       name='PyClone',
-      version='0.9.0',
+      version='0.10.0-b1',
       description='Python tools for analysing clonal evolution using NGS data.',
       author='Andrew Roth',
       author_email='andrewjlroth@gmail.com',
       url='http://compbio.bccrc.ca',
       package_dir = {'': 'lib'},    
       packages=[ 
-                'pyclone'
+                'pyclone',
+                'pyclone.post_process',
+                'pyclone.post_process.plot'
                 ],
       scripts=['PyClone']
      )
