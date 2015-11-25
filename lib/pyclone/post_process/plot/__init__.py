c459e8ee90df24bb5aa4c015354ad34ad2faedf9
aroth85 <aroth85@localhost>
2015-11-25 11:45:26 -0800

    Removed brewer2mpl call

--- lib/pyclone/post_process/plot/__init__.py
+++ lib/pyclone/post_process/plot/__init__.py
@@ -4,12 +4,6 @@ from cellular_frequencies import CellularFrequencyPlot
 from densities import PosteriorDensity
 from similarity_matrix import SimilarityMatrixPlot
 
-import brewer2mpl
-
-bmap = brewer2mpl.get_map('Set1', 'qualitative', 9)
-
-colors = bmap.mpl_colormap
-
 def plot_similarity_matrix(trace_file, plot_file, burnin, thin):
     trace = load_cluster_labels_trace(trace_file, burnin, thin)
     
@@ -25,7 +19,7 @@ def plot_similarity_matrix(trace_file, plot_file, burnin, thin):
 def plot_cellular_frequencies(trace_file, plot_file, burnin, thin):
     trace = load_cellular_frequencies_trace(trace_file, burnin, thin)
         
-    plotter = CellularFrequencyPlot(trace, cmap=colors)
+    plotter = CellularFrequencyPlot(trace)
 
     plotter.plot()
     
